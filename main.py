d0cd2d85d6f4d0ffc53857e1547518481e9b1fb1
Gavrilov. E <Egor4it@yandex.ru>
2021-12-04 16:43:46 +0500

    second trouble

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from statistics import mean
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -6,6 +7,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.li = []
 
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
@@ -18,6 +20,23 @@ class Student:
                 lecturer.le_grade[course] = [le_grade]
         else:
             return 'Ошибка'
+
+    def avr(self):
+        av_g = []
+        for key, value in self.grades.items():
+            av_g.append(mean(value))
+        return mean(av_g)
+
+    def __str__(self):
+        res = f'Имя: {self.name} \n' \
+              f'Фамилия: {self.surname}\n'\
+              f'Средняя оценка за предмет: {self.avr()}\n' \
+              f'Курсы в процессе изучения: {self.courses_in_progress}\n' \
+              f'Завершенные курсы: {self.finished_courses}'
+
+        return res
+
+
 
 
 class Mentor:
@@ -32,6 +51,19 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.le_grade = {}
 
+    def avg(self):
+        av_g = []
+        for value in self.le_grade.values():
+            av_g.append(mean(value))
+        return mean(av_g)
+
+
+    def __str__(self):
+        res = f'Имя: {self.name} \n' \
+              f'Фамилия: {self.surname}\n'\
+              f'Средняя оценка за лекции: {self.avg()}'
+        return res
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -43,21 +75,66 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        res = f'Имя: {self.name} \n' \
+              f'Фамилия: {self.surname}'
+        return res
+
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Git']
+best_student.finished_courses += ['Введение в программирование']
+student_1 = Student('Eliot', 'Mann', 'your_gender')
+student_1.courses_in_progress += ['Python']
+student_1.courses_in_progress += ['Git']
+student_2 = Student('Alex', 'Moor', 'your_gender')
+student_2.courses_in_progress += ['Python']
+student_2.courses_in_progress += ['Git']
 
 cool_Reviewer = Reviewer('Some', 'Buddy')
 cool_Reviewer.courses_attached += ['Python']
+Reviewer_1 = Reviewer('Лев', 'Иванов')
+Reviewer_1.courses_attached += ['Python']
+Reviewer_1.courses_attached += ['Git']
+Reviewer_2 = Reviewer('Иван', 'Петров')
+Reviewer_2.courses_attached += ['Python']
+Reviewer_2.courses_attached += ['Git']
 
 cool_Reviewer.rate_hw(best_student, 'Python', 10)
-cool_Reviewer.rate_hw(best_student, 'Python', 10)
-cool_Reviewer.rate_hw(best_student, 'Python', 10)
+Reviewer_1.rate_hw(student_1, 'Python', 2)
+Reviewer_2.rate_hw(student_2, 'Python', 5)
+Reviewer_2.rate_hw(student_2, 'Git', 9)
 
 cool_lecturer = Lecturer('Some', 'Buddy')
 cool_lecturer.courses_attached += ['Python']
-Student.rate_le(cool_lecturer, 'Python', 10)
-Student.rate_le(cool_lecturer, 'Python', 10)
-Student.rate_le(cool_lecturer, 'Python', 10)
+cool_lecturer.courses_attached += ['Git']
+lecturer_1 = Lecturer('Олег', 'Бубнов')
+lecturer_1.courses_attached += ['Python']
+lecturer_1.courses_attached += ['Git']
+lecturer_2 = Lecturer('Глеб', 'Афанасьев')
+lecturer_2.courses_attached += ['Python']
+lecturer_2.courses_attached += ['Git']
 
-print(best_student.grades)
+best_student.rate_le(cool_lecturer, 'Python', 8)
+student_1.rate_le(lecturer_1, 'Python', 10)
+student_2.rate_le(lecturer_2, 'Python', 9)
+best_student.rate_le(cool_lecturer, 'Git', 1)
+student_1.rate_le(lecturer_1, 'Git', 2)
+student_2.rate_le(lecturer_2, 'Git', 3)
+
+# print('Лекторы')
+# print(cool_lecturer)
+# print(lecturer_1)
+# print(lecturer_2)
+# print()
+# print('Ревьюеры')
+# print(cool_Reviewer)
+# print(Reviewer_1)
+# print(Reviewer_2)
+# print('Студенты')
+# print()
+print(best_student)
+print(student_1)
+print(student_2)
+
